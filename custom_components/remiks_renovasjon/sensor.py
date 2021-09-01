945ce7125260574f625199d95354bece6b6c498f
remimikalsen <remimikalsen@users.noreply.github.com>
2021-09-01 19:15:29 +0200

    Adding debug messages

--- custom_components/remiks_renovasjon/sensor.py
+++ custom_components/remiks_renovasjon/sensor.py
@@ -17,10 +17,10 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         RemiksRenovasjonSensor(remiks_renovasjon, event[0]) for event in remiks_renovasjon.get_tracked_event()
     )
 
-
 class RemiksRenovasjonSensor(Entity):
     def __init__(self, remiks_renovasjon, event_code):
         """Initialize with API object, device id."""
+        _LOGGER.debug("Adding event code: " + event_code)
         self._remiks_renovasjon = remiks_renovasjon
         self._event_code = event_code
 
@@ -29,14 +29,20 @@ class RemiksRenovasjonSensor(Entity):
         """Return the name of the event_code if any."""
         event = self._remiks_renovasjon.get_tracked_event(self._event_code)
         if event is not None:
+            _LOGGER.debug("Name of event code: " + self._event_code + ": " + event[1])
             return event[1]
+        else:
+            _LOGGER.debug("No name for event code: " + self._event_code)
 
     @property
     def state(self):
         """Return the state/date of the event."""
         event = self._remiks_renovasjon.get_tracked_event(self._event_code)
         if event is not None:
+            _LOGGER.debug("State of event code: " + self._event_code + ": " + event[2])
             return event[2].strftime('%d. %b %Y')
+        else:
+            _LOGGER.debug("No state for event code: " + self._event_code)
 
     @property
     def entity_picture(self):
@@ -44,7 +50,10 @@ class RemiksRenovasjonSensor(Entity):
         return None
         event = self._remiks_renovasjon.get_tracked_event(self._event_code)
         if event is not None:
+            _LOGGER.debug("Symbol of event code: " + self._event_code + ": " + event[3])
             return event[3]
+        else:
+            _LOGGER.debug("No symbol for event code: " + self._event_code)
 
     def update(self):
         """Update list of tracked events."""
